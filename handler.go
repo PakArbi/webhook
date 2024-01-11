f34845aba7e9a18b7b7ecd23c9485d9e0ae58152
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2024-01-11 08:35:32 +0700

    new uploade for webhook

---
+++ handler.go
@@ -0,0 +1,134 @@
+package webhook
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aiteung/atapi"
+	"github.com/aiteung/atmessage"
+	"github.com/aiteung/module/model"
+	"github.com/whatsauth/wa"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func PostBalasan(w http.ResponseWriter, r *http.Request) {
+	var msg model.IteungMessage
+	var resp atmessage.Response
+	json.NewDecoder(r.Body).Decode(&msg)
+	link := "https://medium.com/@sidiqfaisal30/cara-generate-code-qr-di-app-pakarbi-2d485478fe14"
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		if msg.Message == "loc" || msg.Message == "Loc" || msg.Message == "lokasi" || msg.LiveLoc {
+			location, err := ReverseGeocode(msg.Latitude, msg.Longitude)
+			if err != nil {
+				// Handle the error (e.g., log it) and set a default location name
+				location = "Unknown Location"
+			}
+
+			reply := fmt.Sprintf("Anyong Aseoo kamu pasti lagi di %s \n Koordinatenya : %s - %s\n Cara Penggunaan WhatsAuth Ada di link dibawah ini"+
+				"yaa kak %s\n", location,
+				strconv.Itoa(int(msg.Longitude)), strconv.Itoa(int(msg.Latitude)), link)
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: reply,
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+
+		} else if msg.Message == "Babi" || msg.Message == "Anjing" || msg.Message == "goblok" {
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: fmt.Sprintf("Ihh kakak %s kamu kasar bangett, aku jadi takut tauuu", msg.Alias_name),
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+
+		} else if msg.Message == "cantik" || msg.Message == "ganteng" || msg.Message == "cakep" {
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: fmt.Sprintf("makasiihh kakak %s kamu jugaa cakep kooo", msg.Alias_name),
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+
+		} else {
+			randm := []string{
+				"nihao, HI bro " + msg.Alias_name + "\n Faisal-Kun  lagi sok sibuk \n I bot PakArbi nice to meet you, semoga kendaraan Anda tetap Aman. <3 \n Cara Generate code QR di app PakArbi ada di link berikut ini ya kak/teh...\n" + link,
+				"Asstghfirullah. Please Don't SPAM nanti Anda kena banned oleh pihak Whatsapp",
+				"hayyyaa ola ini lagi sibuk ",
+				"kak/teh cakep banget tau",
+				"Malam tak seindah ketika membayangimu ketika membuat website.",
+			}
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: GetRandomString(randm),
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+		}
+	} else {
+		resp.Response = "Secret Salah"
+	}
+	fmt.Fprintf(w, resp.Response)
+}
+
+func ReverseGeocode(latitude, longitude float64) (string, error) {
+	// OSM Nominatim API endpoint
+	apiURL := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&lat=%f&lon=%f", latitude, longitude)
+
+	// Make a GET request to the API
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Decode the response
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+
+	// Extract the place name from the response
+	displayName, ok := result["display_name"].(string)
+	if !ok {
+		return "", fmt.Errorf("unable to extract display_name from the API response")
+	}
+
+	return displayName, nil
+}
+
+func Liveloc(w http.ResponseWriter, r *http.Request) {
+	var msg model.IteungMessage
+	var resp atmessage.Response
+	json.NewDecoder(r.Body).Decode(&msg)
+
+	// Reverse geocode to get the place name
+	location, err := ReverseGeocode(msg.Latitude, msg.Longitude)
+	if err != nil {
+		// Handle the error (e.g., log it) and set a default location name
+		location = "Unknown Location"
+	}
+
+	reply := fmt.Sprintf("Anyong Aseoo kamu pasti lagi di %s \n Koordinatenya : %s - %s\n", location,
+		strconv.Itoa(int(msg.Longitude)), strconv.Itoa(int(msg.Latitude)))
+
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		dt := &wa.TextMessage{
+			To:       msg.Phone_number,
+			IsGroup:  false,
+			Messages: reply,
+		}
+		resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://cloud.wa.my.id/api/send/message/text")
+	} else {
+		resp.Response = "Secret Salah"
+	}
+	fmt.Fprintf(w, resp.Response)
+}
+
+func GetRandomString(strings []string) string {
+	randomIndex := rand.Intn(len(strings))
+	return strings[randomIndex]
+}
---
+++ type.go
@@ -0,0 +1,13 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"Asstghfirullah. Please Don't SPAM nanti Anda kena banned oleh pihak Whatsapp",
+	"Kak/teh cakep banget tau",
+	"hayyyaa ola ini lagi sibuk",
+	"Malam tak seindah ketika membayangimu lagi tersenyum asyiq.",
+	"heyo, ni hao, HI arigatou Faisal-Kun lagi sok sibuk",
+}
